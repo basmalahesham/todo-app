d38c215880e0e73e77daba14de03335ab7602838
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-11-25 23:09:15 +0200

    add floatingActionButton & BottomAppBar in home_layout.dart

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
-class HomeLayoutView extends StatelessWidget {
+class HomeLayoutView extends StatefulWidget {
   const HomeLayoutView({super.key});
   static const String routeName = "home_layout";
 
   @override
+  State<HomeLayoutView> createState() => _HomeLayoutViewState();
+}
+
+class _HomeLayoutViewState extends State<HomeLayoutView> {
+  int selectedIndex = 0;
+
+  List<Widget> screens = [Container(), Container()];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var mediaQuery = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: screens[selectedIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 5,
+        tooltip: 'Increment',
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        height: mediaQuery.height * .093,
+        padding: EdgeInsets.zero,
+        notchMargin: 8,
+        color: Colors.white,
+        shape: const CircularNotchedRectangle(),
+        elevation: 0,
+        child: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "settings"),
+          ],
+        ),
+      ),
+    );
   }
 }
