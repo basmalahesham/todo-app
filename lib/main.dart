1bd30afa9c9a7b7ec1310553592f13ba0121f266
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2025-01-16 15:51:09 +0200

    make EasyLoading & bot_toast work

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:untitled3/core/theme/app_theme.dart';
 import 'package:untitled3/layout/home_layout.dart';
 import 'package:untitled3/moduls/splash/splash_view.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
@@ -28,6 +29,9 @@ class MyApp extends StatelessWidget {
         SplashView.routeName: (context) => const SplashView(),
         HomeLayoutView.routeName: (context) => const HomeLayoutView(),
       },
+      builder: EasyLoading.init(
+        builder: BotToastInit(),
+      ),
     );
   }
 }
